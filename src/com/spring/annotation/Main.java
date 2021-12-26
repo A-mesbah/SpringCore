aabbaf9c11b254458da4df55c7caae446402d194
amesbah <mesbah@inmedipsp.de>
2021-12-26 12:38:22 +0100

    spring annotation @Component

---
+++ src/com/spring/annotation/Main.java
@@ -0,0 +1,34 @@
+package com.spring.annotation;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /**
+         * make sure that the container File Already exists
+         */
+        File file = new File("src/com/spring/annotation/container.xml");
+        System.out.println("File Exist : " + file.exists());
+        /**
+         *create new Object From ClassPathXmlApplicationContext
+         */
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/spring/annotation/container.xml");
+        /**
+         get a new object from master Class where
+         * @parm master : this is the name of the Bean that you can give to in component annotation
+         */
+        MasterInterface ms = context.getBean("master", MasterInterface.class);
+
+        ms.name();
+
+
+        context.close();
+
+
+    }
+
+}
---
+++ src/com/spring/annotation/Master.java
@@ -0,0 +1,11 @@
+package com.spring.annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component("master")
+public class Master implements MasterInterface {
+    @Override
+    public void name() {
+        System.out.println("Hallo From Master");
+    }
+}
