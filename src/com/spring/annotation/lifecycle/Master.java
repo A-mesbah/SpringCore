3bc19065d1ff5d5650ed77faf19fa560c4203d4a
amesbah <mesbah@inmedipsp.de>
2021-12-28 11:42:07 +0100

    "spring annotation @PostConstruct,@PreDestroy

--- src/com/spring/annotation/lifecycle/Master.java
+++ src/com/spring/annotation/lifecycle/Master.java
@@ -2,10 +2,30 @@ package com.spring.annotation.lifecycle;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+
 @Component("master")
 public class Master implements MasterInterface {
     @Override
     public void name() {
         System.out.println("Hallo From Master");
     }
+    @PostConstruct
+    /**
+     * This Method wil be applied before initialize Bean of Type master
+     */
+    public void beforeBeanInitialization(){
+        System.out.println("Before Bean Initialization");
+    }
+    @PreDestroy
+    /**
+     * This Method wil be applied before destroy  Bean of Type master
+     */
+    public void beforeBeanDestroying (){
+        System.out.println("Before Bean Destroying");
+    }
+
+
 }
